b339c9a9fe916bfefb9734dc5a71c673fef6b337
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-17 19:29:19 +0000

    refactored composer-char-counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,12 @@
 $(document).ready(function() {
 
-  const characterLimit = 140;
+  const text = $('#tweet-text');
+  const counter = $('.counter');
 
-  const displayCounterLimit = (number, charLimit) => {
-    //store element to variable
-    const element = $('.counter');
-    //take in a number and style elements based on conditions
-    if (number <= charLimit) {
-      if (element.hasClass('over-limit')) {
-        element.removeClass('over-limit')  
-      };
-      element.prop('color', 'var(--black)');
-      element.text(number);
-    } else {
-      if (!element.hasClass('over-limit')) {
-        element.addClass('over-limit')  
-      };
-      element.prop('color', 'red');
-      element.text(charLimit - number);
-    }
-  };
-
-  // --- our code goes here ---
-  $('#tweet-text').on('input', ((event) => {
+  text.on('input', (() => {
     //check the length of the text
-    const counter = event.target.value.length;
-    displayCounterLimit(counter, characterLimit);
+    counter.val(140 - text.val().length);
+    counter.val() <= 0 ? counter.css('color', 'red'): counter.css('color', 'var(--black)');
   }));
 
 

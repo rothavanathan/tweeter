7fbcb2148c6788b86a22ac44358c6b2be830532e
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-17 18:58:02 +0000

    counter output working as expected.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
-  
+
+  const characterLimit = 140;
+
+  const displayCounterLimit = (number, charLimit) => {
+    //store element to variable
+    const element = $('.counter');
+    //take in a number and style elements based on conditions
+    if (number <= charLimit) {
+      if (element.hasClass('over-limit')) {
+        element.removeClass('over-limit')  
+      };
+      element.prop('color', 'var(--black)');
+      element.text(number);
+    } else {
+      if (!element.hasClass('over-limit')) {
+        element.addClass('over-limit')  
+      };
+      element.prop('color', 'red');
+      element.text(charLimit - number);
+    }
+  };
+
   // --- our code goes here ---
   $('#tweet-text').on('input', ((event) => {
     //check the length of the text
     const counter = event.target.value.length;
-    console.log(counter);
+    displayCounterLimit(counter, characterLimit);
+  }));
 
-  }))
+
 });

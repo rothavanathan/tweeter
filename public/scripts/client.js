2392c341dea14b7eefd797b4bd5fccb2eb76b732
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-18 18:42:21 +0000

    client.js is rendering hardcoded tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// const jquery223Min = require("../vendor/jquery-2.2.3.min");
+// const $ = jquery223Min;
+
+
+$(document).ready(function() {
+
+
+// Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      const $tweet = createTweetElement(tweet)
+      $('.old-tweets-container').append($tweet);
+    }
+    console.log(`rendered tweets.`);
+  }
+
+  // define a function createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`<article class="tweet">
+    <header>
+      <img src="${tweet.user.avatars}">
+      <p> ${tweet.user.name}</p>
+      <p class="handle hide">${tweet.user.handle}</p>
+      
+    </header>
+    <main>
+      <p class="old-tweet-text">${tweet.content.text}</p>
+    </main>
+    <footer>
+        <p class="timestamp">${tweet.created_at}</p>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-fire-alt"></i>
+        
+    </footer>
+  </article>
+  `)
+
+    return $tweet;
+  }
+
+
+  
+  renderTweets(data);
+})

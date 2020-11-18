6766d882720bbb64ef1bd2f6aeb84707d2d373e9
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-18 20:30:19 +0000

    now using ajax get request for data. css tweaks for tweet background on hove and footer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,31 +11,31 @@
 $(document).ready(function() {
 
 
-// Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+// // Fake data taken from initial-tweets.json
+//   const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -73,9 +73,21 @@ $(document).ready(function() {
     return $tweet;
   }
 
+  const loadTweets = (action) => {
+    $
+      .ajax({
+        url: '/tweets',
+        dataType: 'json'
+      })
+      .then(data => {
+        console.log(`tweets fetched`, data);
+        action(data);
+        return data;
+      })
+      .catch(err => console.log(err))
+  }
 
-  
-  renderTweets(data);
+  loadTweets(renderTweets);
 
   //add an AJAX POST request that sends the form data to the server
   $('form').on('submit', (event) => {

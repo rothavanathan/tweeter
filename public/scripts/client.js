e8ea442584f305793540440310209e285b604e40
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-19 01:07:53 +0000

    cross site scripting is working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,38 +9,45 @@
 
 
 $(document).ready(function() {
-  
+  //helper functions
   const isFormValid = () => {
     const text = $("#tweet-text").val();
     console.log(text);
     if (text.length === 0) {
-      alert('Hmm.. you can\'t submit an empty tweet')
+      alert('Hmm.. you can\'t submit an empty tweet');
       return false;
     } else if (text.length > 140) {
-      alert('Hmm.. you can\'t submit a tweet that big! 140 characters max.')
+      alert('Hmm.. you can\'t submit a tweet that big! 140 characters max.');
       return false;
     } else if (text === null) {
-      alert('Hmmm.. somethings wrong here. Try again with some different input')
+      alert('Hmmm.. somethings wrong here. Try again with some different input');
       return false;
     }
     return true;
-  }
+  };
 
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+  
+  //tweet functions
   //takes in tweets object and renders each tweet to page
   const renderTweets = function(tweets) {
     // loops through tweets
     for (const tweet of tweets) {
       // calls createTweetElement for each tweet
       // takes return value and appends it to the tweets container
-      const $tweet = createTweetElement(tweet)
+      const $tweet = createTweetElement(tweet);
       $('.old-tweets-container').prepend($tweet);
     }
-  }
+  };
 
   const renderNewTweet = function(tweet) {
     const $tweet = createTweetElement(tweet);
     $('.old-tweets-container').prepend($tweet);
-  }
+  };
   
 
   // define a function createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
@@ -48,29 +55,30 @@ $(document).ready(function() {
   const createTweetElement = function(tweet) {
     let $tweet = $(`<article class="tweet">
     <header>
-      <img src="${tweet.user.avatars}" alt="${tweet.user.name}'s avatar">
-      <p> ${tweet.user.name}</p>
-      <p class="handle hide">${tweet.user.handle}</p>
+      <img src="${escape(tweet.user.avatars)}" alt="${escape(tweet.user.name)}'s avatar">
+      <p> ${escape(tweet.user.name)}</p>
+      <p class="handle hide">${escape(tweet.user.handle)}</p>
       
     </header>
     <main>
-      <p class="old-tweet-text">${tweet.content.text}</p>
+     <p class="old-tweet-text">${escape(tweet.content.text)}</p>
     </main>
     <footer>
-        <p class="timestamp">${tweet.created_at}</p>
+        <p class="timestamp">${escape(tweet.created_at)}</p>
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
         <i class="fas fa-fire-alt"></i>
         
     </footer>
   </article>
-  `)
-
+  `);
     return $tweet;
-  }
+  };
+
+ 
 
   //makes an ajax request and returns json data
-  //intention is to to get tweets then render using renderTweets as callback 
+  //intention is to to get tweets then render using renderTweets as callback
   const loadTweets = (action) => {
     $
       .ajax({
@@ -82,8 +90,8 @@ $(document).ready(function() {
         action(data);
         return data;
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   loadTweets(renderTweets);
 
@@ -96,25 +104,25 @@ $(document).ready(function() {
     
     
     if (isFormValid()) {
-       //extract values from form inputs
+      //extract values from form inputs
       $
-      .ajax({
-        url: '/tweets',
-        method: 'POST',
-        data: $('form').serialize()
-      })
-      .then(res => {
+        .ajax({
+          url: '/tweets',
+          method: 'POST',
+          data: $('form').serialize()
+        })
+        .then(res => {
         //reset form textarea
-        $('#tweet-text').val("");
-        //reset counter
-        $(".counter").val(140).css('color', 'var(--black)');
+          $('#tweet-text').val("");
+          //reset counter
+          $(".counter").val(140).css('color', 'var(--black)');
 
-        console.log(`whoo!! we got a post`, res)
-        loadTweets(renderTweets);
-      })
+          console.log(`whoo!! we got a post`, res);
+          loadTweets(renderTweets);
+        })
       
-      .catch(err => console.log(err))
+        .catch(err => console.log(err));
       // alert(`Form input is invalid. Please enter text with a maximum of 140 characters.`)
-    } 
-  })
-})
+    }
+  });
+});

a4c408b702f14c6da289a28ef51fdad17c32a69e
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-20 17:55:27 +0000

    counter now turns red only on negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   text.on('input', (() => {
     //check the length of the text
     counter.val(140 - text.val().length);
-    counter.val() <= 0 ? counter.css('color', 'red'): counter.css('color', 'var(--black)');
+    counter.val() < 0 ? counter.css('color', 'red'): counter.css('color', 'var(--black)');
   }));
 
 

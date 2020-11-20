acf2373ccdeca137efca7894c36aca291124045f
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-20 17:54:37 +0000

    old-tweet footer updated with time elapsed instead of time posted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -41,6 +41,48 @@ $(document).ready(function() {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
+
+  //calculate time passed
+  const timePassed  = (timeStamp) => {
+    const elapsed = Date.now() - timeStamp;
+    
+    //format timeDiff and return a string
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
+
+    if (elapsed <= msPerMinute) {
+      const seconds = Math.round(elapsed / 1000);
+      return `${seconds} second${seconds === 1 ? " " : "s "}ago`;
+    }
+
+    else if (elapsed <= msPerHour) {
+      const minutes = Math.round(elapsed / msPerMinute);
+      return `${minutes} minute${minutes === 1 ? " " : "s "}ago`;
+    }
+
+    else if (elapsed <= msPerDay ) {
+      const hours = Math.round(elapsed / msPerHour);
+      return `${hours} hour${hours === 1 ? " " : "s "}ago`;
+    }
+
+    else if (elapsed <= msPerMonth) {
+      const days = Math.round(elapsed/msPerDay);
+      return `approximately ${days} day${days === 1 ? " " : "s "}ago`;
+    }
+
+    else if (elapsed <= msPerYear) {
+      const months = Math.round(elapsed/msPerMonth);
+      return `approximately ${months} month${months === 1 ? " " : "s "}ago`;
+    }
+
+    else {
+      const years = Math.round(elapsed / msPerYear);
+      return `approximately ${years} year${years === 1 ? " " : "s "}ago`; 
+    }
+  };
   
   
   //      TWEET FUNCTIONS            //
@@ -74,19 +116,18 @@ $(document).ready(function() {
   //takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
   const createTweetElement = function(tweet) {
     //generate date object to parse the timestamp
-    const time = new Date(tweet.created_at);
     let $tweet = $(`<article class="tweet">
     <header>
-      <img src="${escape(tweet.user.avatars)}" alt="${escape(tweet.user.name)}'s avatar">
-      <p> ${escape(tweet.user.name)}</p>
-      <p class="handle hide">${escape(tweet.user.handle)}</p>
+      <img src="${tweet.user.avatars}" alt="${tweet.user.name}'s avatar">
+      <p> ${tweet.user.name}</p>
+      <p class="handle hide">${tweet.user.handle}</p>
       
     </header>
     <main>
      <p class="old-tweet-text">${escape(tweet.content.text)}</p>
     </main>
     <footer>
-        <p class="timestamp">${escape(time.toLocaleString())}</p>
+        <p class="timestamp">${timePassed(tweet.created_at)}</p>
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
         <i class="fas fa-fire-alt"></i>

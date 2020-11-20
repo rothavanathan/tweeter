8d966a7838f57c284234cf6dd8b28a832a5a6336
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-20 20:36:46 +0000

    refactor of composer-char-count to calculate on page refresh

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
   const text = $('#tweet-text');
   const counter = $('.counter');
 
-  text.on('input', (() => {
-    //check the length of the text
+  const charCount = () => {
     counter.val(140 - text.val().length);
     counter.val() < 0 ? counter.css('color', 'red'): counter.css('color', 'var(--black)');
+  }
+
+  //run charCount on text-area input
+  text.on('input', (() => {
+    //check the length of the text
+    charCount();
   }));
 
+  //call charCount on page load in case there's a value in textarea already
+  charCount();
 });
